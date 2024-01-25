b5d6cf1e985688cf8baaf9c0bcde089cc2594984
eugene9851 <eugene9851@naver.com>
2024-01-25 14:25:39 +0900

    form submit 기본동작 디버깅

--- 이유진/index.js
+++ 이유진/index.js
@@ -1,16 +1,20 @@
 const email = document.querySelector('#email')
 const errorMessage = document.querySelector('.error')
+const form = document.querySelector('form')
 
 function emailCheck(e) {
-  let email_regex = /([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/
-  if(e.target.value!=="" && !email_regex.test(e.target.value)) {
+  e.preventDefault()
+  const EMAIL_REGEX = /([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+  if(!EMAIL_REGEX.test(email.value)) {
     errorMessage.textContent='Valid email required'
     email.classList.add('errorEmail')
+    return;
   } else{
     errorMessage.textContent=''
     email.classList.remove('errorEmail')
+    location.href="./subscribe/subscribe.html"
   }
 }
 
 
-email.addEventListener('change', emailCheck)
+form.addEventListener('submit', emailCheck)
